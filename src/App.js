56a6b6dc42f723d6d4c34517fef4c11083c460aa
buchi-1996 <ukaigweonyebuchi@gmail.com>
2021-08-24 21:13:51 +0100

    project begins

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center py-4 text-2xl font-bold ">Hello React Folks</h1>
+    </div>
+  );
+}
+
+export default App;

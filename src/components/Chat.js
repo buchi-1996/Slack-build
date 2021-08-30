76095bdb230bd1cf28ec0360b42fa2d976313e21
buchi-1996 <ukaigweonyebuchi@gmail.com>
2021-08-30 04:27:25 +0100

    responsive chatbox

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -49,7 +49,7 @@ const Chat = ({ isDrawerOpen, setIsDrawerOpen, isMobile, setIsMobile }) => {
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
                                 </svg>
                             </button>
-                            <div className="flex flex-row items-center space-x-3 pl-6 text-gray-500 pr-3 truncate w-44 min-w-full">
+                            <div className="flex flex-row items-center space-x-3 pl-6 text-gray-500 pr-3 truncate w-16 md:w-auto sm:min-w-full">
                                 <button>
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
